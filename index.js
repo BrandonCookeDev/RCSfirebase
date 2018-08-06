da5fec9d1a0d6eefed62b5b82b1869ea38520c63
github.user-at-576386355408 <brandoncookedev@gmail.com>
2018-08-06 12:38:20 -0400

    Got players to print to the index page

--- index.js
+++ index.js
@@ -31,50 +31,94 @@ function getStaffPicture(staffObj){
 function getPlayerPicture(playerObj){
 	return storage.ref().child('Player/' + playerObj.Picture).getDownloadURL()
 		.then(url => {
-			staffObj.PictureUrl = url;
-			return (staffObj);
+			playerObj.PictureUrl = url;
+			return (playerObj);
 		})
 		.catch(e => {
 			console.warn(e);
-			return staffObj;
+			return playerObj;
 		})
 }
 
-function getTeam(){
-	let getTeam = database.ref('/Team/Staff').orderByKey();
-	getTeam.on('value', function(snapshot){
-		let results = toArray(snapshot.val()) || [];
+function updateStaffDiv(staff){
+	let promises = [];
+	promises = staff.map(member => {
+		return getStaffPicture(member);
+	})
 
-		let promises = [];
-		promises = results.map(member => {
-			return getStaffPicture(member);
-		})
-
-		Promise.all(promises)
-			.then(members => {
+	Promise.all(promises)
+		.then(members => {
 
 			let divs = members.map(member => {
 				let tag = member.key;
 				return `
-				<div>
-					<h4>${tag}</h4>
-					<span>
-						<img src="${member.PictureUrl}" height="100px" />
-					</span>
-					<span>
+				<h4>${tag}</h4>
+				<div class="row">
+					<div class="col-md-6">
+						<img src="${member.PictureUrl}" class="staffPic" />
+					</div>
+					<div class="col-md-6">
 						<ul>
 							<li><label>Name: </label> ${member.Name}</li>
+							<li><label>Position: </label> ${member.Position}</li>
 							<li><label>Twitter: </label> <a href="twitter.com/${member.Twitter}">@${member.Twitter}</a></li>
 							<li><label>Bio: </label> ${member.Bio}</li>
 						</ul>
-					</span>
+					</div>
 				</div>
 				`;
 			})
-
 			staffDiv.innerHTML = divs;
+
 		}).catch(console.error);
+}
+
+function updatePlayersDiv(players){
+	let promises = [];
+	promises = players.map(member => {
+		return getPlayerPicture(member);
+	})
+
+	Promise.all(promises)
+		.then(members => {
+			let divs = members.map(member => {
+				let tag = member.key;
+				return `
+				<h4>${tag}</h4>
+				<div class="row">
+					<div class="col-md-6">
+						<img src="${member.PictureUrl}" class="playerPic" />
+					</div>
+					<div class="col-md-6">
+						<ul>
+							<li><label>Name: </label> ${member.Name}</li>
+							<li><label>Twitter: </label> @${member.Twitter}</li>
+							<li><label>Bio: </label> ${member.Bio}</li>
+						</ul>
+					</div>
+				</div>
+				`
+			})
+			playerDiv.innerHTML = divs;
+		})
+		.catch(console.error)
+}
+
+function getTeam(){
+	let team = database.ref('/Team/Staff').orderByKey();
+	team.on('value', function(snapshot){
+		let results = toArray(snapshot.val()) || [];
+		updateStaffDiv(results);
+	})
+}
+
+function getPlayers(){
+	let players = database.ref('/Team/Players').orderByKey();
+	players.on('value', function(snapshot){
+		let results = toArray(snapshot.val()) || [];
+		updatePlayersDiv(results);
 	})
 }
 
 getTeam();
+getPlayers();
